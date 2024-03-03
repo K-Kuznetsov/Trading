f38cad506f8dc390330035af6fd2733d4c3ef6c9
K-Kuznetsov <kimmok1990@gmail.com>
2024-03-03 17:23:25 +0200

    Added function to combine 2 portfolios into one object

--- Start.ts
+++ Start.ts
@@ -1,11 +1,11 @@
 import 'dotenv/config';
 import BinanceData from './BinanceCCXT';
 import KucoinData from './KucoinCCXT';
+import CombinedWallet from './Portfolio';
 
 const BinanceAPIkey: string = process.env.BinanceAPIkey || '';
 const BinanceAPIsecret: string = process.env.BinanceAPIsecret || '';
 const BinancePassword: string = process.env.BinancePassword || '';
-
 const KucoinAPIkey: string = process.env.KucoinAPIkey || '';
 const KucoinAPIsecret: string = process.env.KucoinAPIsecret || '';
 const KucoinPassword: string = process.env.KucoinPassword || '';
@@ -13,9 +13,8 @@ const KucoinPassword: string = process.env.KucoinPassword || '';
 const main = async () => {
     const BinanceWallet = await BinanceData(BinanceAPIkey, BinanceAPIsecret, BinancePassword);
     const KucoinWallet = await KucoinData(KucoinAPIkey, KucoinAPIsecret, KucoinPassword);
-
-    console.log(BinanceWallet);
-    console.log(KucoinWallet);
+    const Portfolio = CombinedWallet(BinanceWallet, KucoinWallet);
+    console.log(Portfolio);
 };
 
 main();
